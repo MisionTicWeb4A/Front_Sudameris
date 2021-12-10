09c1ce7c100ec94ddad97aaca923a88cb0220caa
Kecatoca <kecatocamix@hotmail.com>
2021-12-10 00:57:07 -0500

    Simulacion de Api

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ import PQRs from './components/PQRs';
 
 function App() {
 
+  const vec_api = [{id:1, fecha: '2020-01-01', detalle:'Retiro ATH', monto: '100.00'},
+    {id:2, fecha: '2020-01-02', detalle:'Compra Alkosto', monto: '200.00'},
+    {id:3, fecha: '2020-01-03', detalle:'Compra SAO 92', monto: '300.00'},
+    {id:4, fecha: '2020-01-04', detalle:'Transferencia', monto: '400.00'},
+    {id:5, fecha: '2020-01-05', detalle:'Pago cuota', monto: '500.00'}
+  ];
+
   const [ compDinamico, setCompDinamico ] = useState(<Banner />);
 
   const onOptionClicked = function (evt) {
@@ -27,7 +34,7 @@ function App() {
       else { 
         if (evt.target.name==="Registrarse") {setCompDinamico(<Signin />);}
         else {
-          if (evt.target.name==="Extractos") {setCompDinamico(<Extractos />);} 
+          if (evt.target.name==="Extractos") {setCompDinamico(<Extractos datos= { vec_api } />);} 
           else {
             if (evt.target.name==="Transferencias") {setCompDinamico(<Transferencias />);} 
             else {
--- src/components/Extractos.jsx
+++ src/components/Extractos.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import '../style.css'
 import Table from 'react-bootstrap/Table'
 
-function Extractos() {
+function Extractos(props) {
+   
+    
     return (
         <div className="contenido">
             <div className="py-5">                  
@@ -19,37 +21,28 @@ function Extractos() {
                                     <option value="3">Marzo</option>
                                 </select>
                                 <button class="btn btn-outline-secondary" type="button">Consultar</button>
+                                    
                             </div>
                         </div>
                     </div>
                 <Table striped bordered hover size="sm">
                         <thead>
                             <tr>
-                            <th>#</th>
+                            <th>Id</th>
                             <th>Fecha</th>
                             <th>Detalle</th>
                             <th>Monto</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                            <td>1</td>
-                            <td>21-01-21</td>
-                            <td>Compra EDS Carretera </td>
-                            <td>$ 30.000.oo</td>
-                            </tr>
-                            <tr>
-                            <td>2</td>
-                            <td>22/01/21</td>
-                            <td>Thornton S.A.</td>
-                            <td>$ 734.000</td>
-                            </tr>
-                            <tr>
-                            <td>3</td>
-                            <td>23/01/21</td>
-                            <td>Subway</td>
-                            <td>$ 14.000</td>
-                            </tr>
+                            {props.datos.map(datos => (                            
+                                <tr>
+                                    <td>{ datos.id }</td>
+                                    <td>{ datos.fecha }</td>
+                                    <td>{ datos.detalle } </td>
+                                    <td>{ datos.monto }</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </Table> 
                 </div>
